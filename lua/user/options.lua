57b80eaad05502d8a750e5d30f01b892d4bee61d
Slayer <jacob.schoemaker2000@gmail.com>
2022-05-19 19:57:35 +0200

    more options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -2,10 +2,22 @@ local options = {
 	backup = false,
 	writebackup = true,
 	belloff = "", 		--always ring bell (for practicing purposes)
+	errorbells = true,
 	browsedir = "last",
 	smartindent = true,
+	clipboard = "unnamedplus",
+	cmdheight = 2,
+	cursorline = true,
+	splitright = true,
+	splitbelow = false,
+	equalalways = true,      --Make all windows the same size. Under consideration for being turned off
+	termguicolors = true,
+	langmenu = "none",
 }
 
 for k, v in pairs(options) do
 	vim.opt[k] = v
 end
+
+vim.cmd [[set diffopt+=horizontal]]
+vim.cmd [[set iskeyword+=-]]
