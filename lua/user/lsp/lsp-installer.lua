ea501c16b2c060b03616abc169a40a90c21e18f4
SlayerOfTheBad <git@mechanical.computer>
2022-06-03 08:28:38 +0200

    Lsp Configurations

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -4,8 +4,6 @@ if not status_ok then
 end
 
 local opts = {
-  on_attach = require("user.lsp.handlers").on_attach,
-  capabilities = require("user.lsp.handlers").capabilities,
   ensure_installed = {
     "rust_analyzer",
     "phpactor",
@@ -17,36 +15,3 @@ local opts = {
 lsp_installer.setup(opts);
 
 require "user.lsp.settings"
--- if server.name == "jsonls" then
---   local jsonls_opts = require("user.lsp.settings.jsonls")
---   opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
--- end
-
--- if server.name == "sumneko_lua" then
---   local sumneko_opts = require("user.lsp.settings.sumneko_lua")
---   opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
--- end
-
--- lsp_installer.on_server_ready(function(server)
---   local opts = {
---     on_attach = require("user.lsp.handlers").on_attach,
---     capabilities = require("user.lsp.handlers").capabilities,
---     ensure_installed = {
---       "rust_analyzer",
---       "phpactor",
---       "eslint",
---     }
---   }
-
---   if server.name == "jsonls" then
---     local jsonls_opts = require("user.lsp.settings.jsonls")
---     opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
---   end
-
---   if server.name == "sumneko_lua" then
---     local sumneko_opts = require("user.lsp.settings.sumneko_lua")
---     opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
---   end
-
---   server:setup(opts)
--- end)
