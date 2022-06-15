af4851176daf8488826f6896220ab29adf1d80b4
SlayerOfTheBad <git@mechanical.computer>
2022-06-15 11:31:44 +0200

    Fix ESlint formatting

--- lua/user/lsp/settings/eslint.lua
+++ lua/user/lsp/settings/eslint.lua
@@ -1,21 +1,20 @@
+local base_conf = require("user.lsp.settings.base_conf")
+
 local opts = {
-  setup = {
-    commands = {
-      Format = {
-        function()
-          vim.lsp.buf.execute_command({
-            command = 'eslint.applyAllFixes',
-            arguments = {
-              {
-                uri = vim.uri_to_bufnr(bufnr),
-                version = lsp.util.buf_versions[bufnr],
-              }
-            }
-          });
-        end,
-      },
-    },
-  },
+  on_attach = function(client, bufnr)
+    base_conf.on_attach(client, bufnr)
+    vim.api.nvim_buf_create_user_command(bufnr, "Format", function ()
+      vim.lsp.buf.execute_command({
+        command = 'eslint.applyAllFixes',
+        arguments = {
+          {
+            uri = vim.uri_from_bufnr(bufnr),
+            version = vim.lsp.util.buf_versions[bufnr],
+          },
+        },
+      })
+    end, {})
+  end,
 }
 
 return opts
