1496f75e70369b4e9887321cad6d81e1c1160a0b
Jacob Schoemaker <jacob.schoemaker2000@gmail.com>
2022-07-26 16:58:05 +0200

    Add Tagbar and vim-airline

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,44 +1,47 @@
 local options = {
-    backup = false,
-    writebackup = true,
-    belloff = "",           --always ring bell (for practicing purposes)
-    errorbells = true,
-    browsedir = "last",
-    clipboard = "unnamedplus",
-    cmdheight = 2,
-    cursorline = true,
-    splitright = true,
-    splitbelow = false,
-    equalalways = true,      --Make all windows the same size. Under consideration for being turned off
-    termguicolors = true,
-    langmenu = "none",
-    listchars = "tab:> ,trail:-,nbsp:+",
-    list = true,
-    number = true,
-    relativenumber = true,
-    expandtab = true,
-    shiftwidth = 2,
-    tabstop = 4,
-    ruler = true,
-    scrolloff = 5,
-    sidescrolloff = 7,
-    showbreak = "-> ",
-    smartindent = true,
-    smarttab = true,
-    wildmode = "longest:full,full",
-    exrc = true,
-    secure = true,
-    foldlevel = 2,
+  backup = false,
+  writebackup = true,
+  belloff = "",           --always ring bell (for practicing purposes)
+  errorbells = true,
+  browsedir = "last",
+  clipboard = "unnamedplus",
+  cmdheight = 2,
+  cursorline = true,
+  splitright = true,
+  splitbelow = false,
+  equalalways = true,      --Make all windows the same size. Under consideration for being turned off
+  termguicolors = true,
+  langmenu = "none",
+  listchars = "tab:> ,trail:-,nbsp:+",
+  list = true,
+  number = true,
+  relativenumber = true,
+  expandtab = true,
+  shiftwidth = 2,
+  tabstop = 4,
+  ruler = true,
+  scrolloff = 5,
+  sidescrolloff = 7,
+  showbreak = "-> ",
+  showmode = false,
+  smartindent = true,
+  smarttab = true,
+  wildmode = "longest:full,full",
+  exrc = true,
+  secure = true,
+  foldlevel = 2,
 }
 
 for k, v in pairs(options) do
-        vim.opt[k] = v
+  vim.opt[k] = v
 end
+
+vim.g.airline_theme='base16_gruvbox_dark_hard'
+vim.g.airline_powerline_fonts=1
+vim.g["airline#extensions#tabline#enabled"]=1
 
 vim.cmd [[set exrc]]
 -- vim.cmd [[syntax enable]]
 vim.cmd [[set diffopt+=horizontal]]
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set matchpairs+=<:>]]
-vim.cmd [[set statusline=%{FugitiveStatusline()}\ %<%f\ %h%m%r%=%-14.(%l,%c%V%)\ %P]]
-vim.cmd [[set statusline+=%{gutentags#statusline('\ [',']')}]]
