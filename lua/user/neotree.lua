07e77edf4de946df990ed3656bb23de0fa9a1f08
Jacob <j.schoemaker@alumio.com>
2024-01-25 09:22:29 +0100

    Remove project-specific dap config
    Also some other small changes.

--- lua/user/neotree.lua
+++ lua/user/neotree.lua
@@ -33,15 +33,44 @@ neotree.setup({
       },
     },
   },
+  document_symbols = {
+    kinds = {
+      File = { icon = "󰈙", hl = "Tag" },
+      Namespace = { icon = "󰌗", hl = "Include" },
+      Package = { icon = "󰏖", hl = "Label" },
+      Class = { icon = "󰌗", hl = "Include" },
+      Property = { icon = "󰆧", hl = "@property" },
+      Enum = { icon = "󰒻", hl = "@number" },
+      Function = { icon = "󰊕", hl = "Function" },
+      String = { icon = "󰀬", hl = "String" },
+      Number = { icon = "󰎠", hl = "Number" },
+      Array = { icon = "󰅪", hl = "Type" },
+      Object = { icon = "󰅩", hl = "Type" },
+      Key = { icon = "󰌋", hl = "" },
+      Struct = { icon = "󰌗", hl = "Type" },
+      Operator = { icon = "󰆕", hl = "Operator" },
+      TypeParameter = { icon = "󰊄", hl = "Type" },
+      StaticMethod = { icon = '󰠄 ', hl = 'Function' },
+    }
+  },
+  -- Add this section only if you've configured source selector.
+  source_selector = {
+    sources = {
+      { source = "filesystem", display_name = " 󰉓 Files " },
+      { source = "git_status", display_name = " 󰊢 Git " },
+    },
+  },
   buffers = {
     show_unloaded = true,
   },
   filesystem = {
     filtered_items = {
+      hide_dotfiles = false,
+      hide_gitignored = false,
       always_show = {
-        "/vendor/magement",
-        "/vendor/alumio",
-        "/vendor/mediact",
+        "vendor/magement",
+        "vendor/alumio",
+        "vendor/mediact",
       },
     },
     follow_current_file = true,
